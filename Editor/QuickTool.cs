a3a55a71bdf4e4538efa9ca0b67d2485b570cf6f
Grant Denbrock <gdenbroc@asu.edu>
2022-06-09 14:10:05 -0700

    Update QuickTool.cs

--- Editor/QuickTool.cs
+++ Editor/QuickTool.cs
@@ -51,7 +51,7 @@ public class QuickTool : EditorWindow
     
     string[] filePaths = Directory.GetFiles(@"P:/dd-pipeline/Icons/", "*.png"); // get every file in chosen directory with the extension.png
     // write those files out so I can see them
-    Debug.Log(filePaths)
+    Debug.Log(filePaths);
     WWW www = new WWW("file://" + filePaths[0]);                  // "download" the first file from disk
     yield return www;                                                               // Wait unill its loaded
     Texture2D new_texture = new Texture2D(512,512);               // create a new Texture2D (you could use a gloabaly defined array of Texture2D )
